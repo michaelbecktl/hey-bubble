170ffbd002d833740fedcf7b05b57610d3a8cd9f
Michael Beck <michaelbeck1206@gmail.com>
2025-08-19 10:53:10 +1200

    Updated models with UserProfile and Posts

--- frontend/models/models.tsx
+++ frontend/models/models.tsx
@@ -7,3 +7,23 @@ export interface UserCredentials {
 export interface User extends UserCredentials {
   id: number
 }
+
+export interface UserProfile {
+  userId: number
+  displayName: string
+  dob: string
+  profilePhoto?: string
+  gender: string
+  country: string
+  nativeLanguage: string
+  learningLanguage: string
+}
+export interface Post {
+  postId: number
+  userId: number
+  content: string
+  createdAt: string
+  updatedAt: string
+  mediaUrl: string
+  mediaType: string
+}
