b8e6001aad8954ffb616652a80542c885d5df138
Michael Beck <michaelbeck1206@gmail.com>
2025-08-27 10:02:20 +1200

    Changed JWT Token to store relevant claims

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -18,6 +18,7 @@ public class UserController : ControllerBase
 
   public class UserLogin
   {
+    public string? Id { get; set; }
     public required string Username { get; set; }
 
     public required string Password { get; set; }
